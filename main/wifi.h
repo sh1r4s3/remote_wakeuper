39ece046f35ce4112ea3dd452253c831fd6168d4
Nikita Ermakov <sh1r4s3@mail.si-head.nl>
2024-07-31 15:58:58 -0500

    Move WiFi related code to a separate module

---
+++ main/wifi.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Nikita Ermakov <sh1r4s3@mail.si-head.nl>
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef RKW_WIFI
+#define RKW_WIFI
+
+#include "class/hid/hid.h"
+
+void wifi_init();
+
+#endif
